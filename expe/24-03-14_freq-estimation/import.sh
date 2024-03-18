63f0e85124baa859f97de619f39bbe4aaecf880c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-03-18 19:14:33 +0100

    Improve import script for last expe

--- expe/24-03-14_freq-estimation/import.sh
+++ expe/24-03-14_freq-estimation/import.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# On Saren:
-cp /home/drac/storage/dataset/experiments/2024-02-06_firmware-instrumentation-for-phase-rot-leak/nf/FC_128e6_SR_30e6_76db.npy .
+file=FC_128e6_SR_30e6_76db.npy
+
+# DONE: On Saren:
+if [[ ! -f ./$file ]]; then
+    cp /home/drac/storage/dataset/experiments/2024-02-06_firmware-instrumentation-for-phase-rot-leak/nf/$file .
+fi
+
