b9ee8e1196ab9ee33f466e40982b0e73bf070480
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-03-21 19:27:31 +0100

    Add new experiment about recombination vote

---
+++ expe/24-03-21_recombination-vote/template_attack_recombination.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# * Parameters
+
+LOG_OUTPUT="$PATH_PROJ/expe/24-03-21_recombination-vote/output.log"
+LOG_OUTPUT_FILTERED="${LOG_OUTPUT/.log/_filtered.log}"
+
+# Path of dataset used to create the profile.
+TRAIN_SET=$PATH_DATASET/240309_custom_firmware_phase_eval_iq_norep_modgfsk/train
+# Base path used to store the created profile.
+PROFILE_PATH_BASE=$TRAIN_SET/../profile
+# Path of dataset used to perform the attack.
+ATTACK_SET=$PATH_DATASET/240309_custom_firmware_phase_eval_iq_norep_modgfsk/attack
+
+# Number of traces to use for profile creation.
+NUM_TRACES_PROFILE=16000
+# Number of traces to use for attack.
+NUM_TRACES_ATTACK=1000
+# Delimiters. Small window greatly increase profile computation speed.
+START_POINT=0
+END_POINT=0
+
+# NOTE: Sampling rate is hardcoded in collect_*.sh scripts.
+FS=8e6
+
+# * Functions
+
+function attack_recombined() {
+    # bruteforce="--bruteforce"
+    # plot="--plot"
+    echo "========================= $NUM_TRACES_ATTACK attack traces ========================="
+    pois_algo="snr"
+    echo "pois_algo=$pois_algo"
+    export PROFILE_PATH=${PROFILE_PATH_BASE}_{}_${NUM_TRACES_PROFILE}_${pois_algo}
+    sc-attack $plot --norm --data-path $ATTACK_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_ATTACK $bruteforce \
+              attack-recombined $PROFILE_PATH --attack-algo pcc --variable p_xor_k --align --fs $FS
+}
+
+# * Script
+
+# Checkout corresponding branch in Screaming Channels PoC repo
+(cd "$PATH_SCPOC" && git checkout feat-recombination)
+
+# ** Attacks
+
+# DONE: Attack using recombination with major vote.
+attack_recombined | tee -a "$LOG_OUTPUT"
+grep -E "===|Best|Known|HD|SUCCESS|NUMBER" "$LOG_OUTPUT" > $LOG_OUTPUT_FILTERED
