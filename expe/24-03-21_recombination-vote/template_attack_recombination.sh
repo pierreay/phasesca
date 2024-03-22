94d1e4e95a68e8a0dcc70221d39775dda53ced1a
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-03-22 11:55:20 +0100

    Add iterate function to create plots in last experiment using recombination

--- expe/24-03-21_recombination-vote/template_attack_recombination.sh
+++ expe/24-03-21_recombination-vote/template_attack_recombination.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-# * Parameters
+# * Global variables
 
-# ** Paths
-
-PATH_DATASET_EXTERN="$PATH_DATASET/240309_custom_firmware_phase_eval_iq_norep_modgfsk"
-PATH_EXPE="$PATH_PROJ/expe/24-03-21_recombination-vote"
-
-# Output full logs for individual attacks.
-LOG_OUTPUT="$PATH_EXPE/output.log"
-LOG_OUTPUT_FILTERED="${LOG_OUTPUT/.log/_filtered.log}"
-
-# Output CSV files for Python plots.
-LOG_CSV="$PATH_EXPE/attack_results.csv"
-
-# Path of dataset used to create the profile.
-TRAIN_SET="$PATH_DATASET_EXTERN/train"
-# Base path used to store the created profile.
-PROFILE_PATH_BASE="$PATH_DATASET_EXTERN/profile"
-# Path of dataset used to perform the attack.
-ATTACK_SET="$PATH_DATASET_EXTERN/attack"
-
-# ** Numbers
+# ** Parameters
 
 # Number of traces to use for profile creation.
 NUM_TRACES_PROFILE=16000
@@ -34,17 +15,64 @@ END_POINT=0
 # NOTE: Sampling rate is hardcoded in collect_*.sh scripts.
 FS=8e6
 
+# POIs algorithm.
+POIS_ALGO="snr"
+
+# ** Paths
+
+PATH_DATASET_EXTERN="$PATH_DATASET/240309_custom_firmware_phase_eval_iq_norep_modgfsk"
+PATH_EXPE="$PATH_PROJ/expe/24-03-21_recombination-vote"
+
+# Output full logs for individual attacks.
+LOG_OUTPUT="$PATH_EXPE/output.log"
+LOG_OUTPUT_FILTERED="${LOG_OUTPUT/.log/_filtered.log}"
+
+# Output CSV files for Python plots.
+OUTFILE_CSV="$PATH_EXPE/attack_results.csv"
+
+# Path of dataset used to create the profile.
+TRAIN_SET="$PATH_DATASET_EXTERN/train"
+# Base path of the profile.
+PROFILE_PATH_BASE="$PATH_DATASET_EXTERN/profile"
+# Pattern of the used profile.
+PROFILE_PATH="${PROFILE_PATH_BASE}_{}_${NUM_TRACES_PROFILE}_${POIS_ALGO}"
+# Path of dataset used to perform the attack.
+ATTACK_SET="$PATH_DATASET_EXTERN/attack"
+
 # * Functions
 
 function attack_recombined() {
     # bruteforce="--bruteforce"
     # plot="--plot"
     echo "========================= $NUM_TRACES_ATTACK attack traces ========================="
-    pois_algo="snr"
-    echo "pois_algo=$pois_algo"
-    export PROFILE_PATH=${PROFILE_PATH_BASE}_{}_${NUM_TRACES_PROFILE}_${pois_algo}
     sc-attack $plot --norm --data-path $ATTACK_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_ATTACK $bruteforce \
               attack-recombined $PROFILE_PATH --attack-algo pcc --variable p_xor_k --align --fs $FS
+}
+
+function iterate() {
+    i_start=$1
+    i_step=$2
+    i_end=$(($3 - 1 ))
+    # Iteration over number of traces.
+    for num_traces in $(seq $i_start $i_step $i_end); do
+        # Write number of traces.
+        echo -n "$num_traces;" | tee -a "$OUTFILE_CSV"
+
+        # Attack and extract:
+        # 1) The sum of the hamming distance.
+        # 1) The number of correct bytes.
+        sc-attack --no-plot --norm --data-path $ATTACK_SET --start-point $START_POINT --end-point $END_POINT --num-traces $num_traces \
+                  attack-recombined $PROFILE_PATH --attack-algo pcc --variable p_xor_k --align --fs $FS 2>/dev/null \
+            | grep -E 'HD SUM|CORRECT' \
+            | cut -f 2 -d ':' \
+            | tr -d ' ' \
+            | tr '[\n]' '[;]' \
+            | sed 's/2^//' \
+            | sed 's/;$//' \
+            | tee -a "$OUTFILE_CSV"
+
+        echo "" | tee -a "$OUTFILE_CSV"
+    done
 }
 
 # * Script
@@ -55,7 +83,13 @@ function attack_recombined() {
 # ** Attacks
 
 # DONE: Attack using recombination with major vote.
-attack_recombined | tee -a "$LOG_OUTPUT"
-grep -E "===|Best|Known|HD|SUCCESS|NUMBER" "$LOG_OUTPUT" > $LOG_OUTPUT_FILTERED
+# attack_recombined | tee -a "$LOG_OUTPUT"
+# grep -E "===|Best|Known|HD|SUCCESS|NUMBER|template_dir" "$LOG_OUTPUT" > $LOG_OUTPUT_FILTERED
 
 # ** Plot
+
+# WAIT:
+# Create the CSV.
+echo "trace_nb;correct_bytes_amp;hd_sum_amp;correct_bytes_phr;hd_sum_phr;correct_bytes_i_augmented;hd_sum_i_augmented;correct_bytes_q_augmented;hd_sum_q_augmented;" | tee "$OUTFILE_CSV"
+# Iterate over number of traces to attack.
+iterate 10 10 100
