c34381f36ea95bb08433c37690f37c274aa4ecef
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-07-04 21:49:35 +0200

    Update src for new filtering

--- sets/24-07-04_nrf52-ref/src/process.sh
+++ sets/24-07-04_nrf52-ref/src/process.sh
@@ -18,19 +18,20 @@ log_info "Loaded environment: ${env}"
 mkdir -p train_1
 cp -t train_1 train/pt.txt train/key.txt
 scaff --config src/collect.toml extract train train_1 --avg-min=1 --avg-max=1 --cpu=-1 --corr-min=8.5e-1
-
 mkdir -p attack_1
 cp -t attack_1 attack/pt.txt attack/key.txt
 scaff --config src/collect.toml extract attack attack_1 --avg-min=1 --avg-max=1 --cpu=-1 --corr-min=8.5e-1
 
-mkdir -p train_filt
-cp -t train_filt train/pt.txt train/key.txt
-"$(realpath $(dirname $0))/process_filt.py" train train_filt src/collect.toml
-
+mkdir -p train_filt_lh1e6
+cp -t train_filt_lh1e6 train/pt.txt train/key.txt
+"$(realpath $(dirname $0))/process_filt.py" train train_filt_lh1e6 src/collect.toml
 mkdir -p attack_filt_lh1e6
 cp -t attack_filt_lh1e6 attack/pt.txt attack/key.txt
 "$(realpath $(dirname $0))/process_filt.py" attack attack_filt_lh1e6 src/collect.toml lh1e6
 
+mkdir -p train_filt_lh500e3
+cp -t train_filt_lh500e3 train/pt.txt train/key.txt
+"$(realpath $(dirname $0))/process_filt.py" train train_filt_lh500e3 src/collect.toml lh500e3
 mkdir -p attack_filt_lh500e3
 cp -t attack_filt_lh500e3 attack/pt.txt attack/key.txt
 "$(realpath $(dirname $0))/process_filt.py" attack attack_filt_lh500e3 src/collect.toml lh500e3
