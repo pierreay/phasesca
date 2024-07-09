c1beb72be32f98c631e046a94426b3a0472012d3
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-07-09 14:23:18 +0200

    Update stm32-ref dataset

---
+++ sets/24-07-09_stm32l1-ref/src/process.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# * Environment
+
+env="$(realpath $(dirname $0))/env.sh"
+source "$env"
+
+# Safety-guard.
+if [[ -z $ENV_FLAG ]]; then
+    echo "ERROR: Environment can't been sourced!"
+    exit 1
+fi
+
+log_info "Loaded environment: ${env}"
+
+# * Script
+
+mkdir -p train_1
+cp -t train_1 train/pt.txt train/key.txt
+scaff --config src/collect.toml extract train train_1 --avg-min=1 --avg-max=1 --cpu=-1 --corr-min=8.5e-1
+mkdir -p attack_1
+cp -t attack_1 attack/pt.txt attack/key.txt
+scaff --config src/collect.toml extract attack attack_1 --avg-min=1 --avg-max=1 --cpu=-1 --corr-min=8.5e-1
+
+mkdir -p train_filt_lh1e6
+cp -t train_filt_lh1e6 train/pt.txt train/key.txt
+"$(realpath $(dirname $0))/process_filt.py" train train_filt_lh1e6 src/collect.toml
+mkdir -p attack_filt_lh1e6
+cp -t attack_filt_lh1e6 attack/pt.txt attack/key.txt
+"$(realpath $(dirname $0))/process_filt.py" attack attack_filt_lh1e6 src/collect.toml lh1e6
+
+mkdir -p train_filt_lh500e3
+cp -t train_filt_lh500e3 train/pt.txt train/key.txt
+"$(realpath $(dirname $0))/process_filt.py" train train_filt_lh500e3 src/collect.toml lh500e3
+mkdir -p attack_filt_lh500e3
+cp -t attack_filt_lh500e3 attack/pt.txt attack/key.txt
+"$(realpath $(dirname $0))/process_filt.py" attack attack_filt_lh500e3 src/collect.toml lh500e3
+
+mkdir -p train_filt_lh50e3
+cp -t train_filt_lh50e3 train/pt.txt train/key.txt
+"$(realpath $(dirname $0))/process_filt.py" train train_filt_lh50e3 src/collect.toml lh50e3
+mkdir -p attack_filt_lh50e3
+cp -t attack_filt_lh50e3 attack/pt.txt attack/key.txt
+"$(realpath $(dirname $0))/process_filt.py" attack attack_filt_lh50e3 src/collect.toml lh50e3
+
+mkdir -p train_filt_hl50e3
+cp -t train_filt_hl50e3 train/pt.txt train/key.txt
+"$(realpath $(dirname $0))/process_filt.py" train train_filt_hl50e3 src/collect.toml hl50e3
+mkdir -p attack_filt_hl50e3
+cp -t attack_filt_hl50e3 attack/pt.txt attack/key.txt
+"$(realpath $(dirname $0))/process_filt.py" attack attack_filt_hl50e3 src/collect.toml hl50e3
